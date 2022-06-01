947b9f7c32a83c1377eed9b6a11ec6a85d4384e6
Perkles <victor-prates@homtail.com>
2022-06-01 19:58:23 -0300

    Inclusão do desafio Palíndromo

---
+++ Desafio Palíndromo/script.js
@@ -0,0 +1,17 @@
+function verificar_palindromo() {
+    let div_resposta = document.querySelector('#resp')
+    let entrada = document.querySelector('#palavra').value
+
+    if (entrada == undefined || entrada == null || entrada == '') return
+
+    let arrayPalavra = entrada.toUpperCase().split('').filter(element => element != ' ')
+    let arrayInvertido = [...arrayPalavra].reverse()
+
+    let palavra = arrayPalavra.toString()
+    let palavraInvertida = arrayInvertido.toString()
+
+    let teste = palavra == palavraInvertida ? 'é' : 'não é'
+
+    div_resposta.innerHTML = `A palavra/frase \"${entrada}\" ${teste} um palídromo.`
+
+}
